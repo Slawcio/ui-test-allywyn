597d830352e1099f0f80b741ed3402863b94134f
slawomir.stankiewicz <slawomir.stankiewicz@leovegas.com>
2025-06-28 19:47:52 +0200

    maybe this will work

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,13 +30,13 @@ export default defineConfig({
     process.env.CI ? ['github'] : ['line'],
     // ['json', { outputFile: 'playwright-report/report.json' }] // might be needed for integrations
   ],
-  snapshotPathTemplate: 'tests/e2e/snapshots/{projectName}/{testFilePath}/{arg}{ext}',
+  snapshotPathTemplate: '.tests/snapshots/{projectName}/{testFilePath}/{arg}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect: {
     toHaveScreenshot: {
-      threshold: 0.7,
-      maxDiffPixelRatio: 1,
-      maxDiffPixels: 10000,    
+      threshold: 0.3,
+      maxDiffPixelRatio: 0.03,
+      maxDiffPixels: 50,    
     }
   },
   use: {
