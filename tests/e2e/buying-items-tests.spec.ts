e5ace32f5a040c8fb72415e1a56ba68c27129ffe
slawomir.stankiewicz <slawomir.stankiewicz@leovegas.com>
2025-06-28 17:52:04 +0200

    one job, removing console.logs

--- tests/e2e/buying-items-tests.spec.ts
+++ tests/e2e/buying-items-tests.spec.ts
@@ -122,7 +122,6 @@ test.describe('Buying tests', {tag: ['@buy-functionality', '@smoke']},() => {
       // checkout summary
       const checkoutSummary = new CheckoutSummary(page);
       const summaryItemList: ItemView[] = await checkoutSummary.getItemsList(); 
-      console.log('summaryItemList: ', summaryItemList);
       for(let i = 0; i < summaryItemList.length; i++){
         await summaryItemList[0].assertProductDataAccuracyInRow(productsList[0]);
       }
